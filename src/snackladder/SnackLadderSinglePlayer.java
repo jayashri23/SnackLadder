11b561f0e1d0c0e00972adf08c45a1c52e0541bc
jayashri23 <jsjankar23@gmail.com>
2022-10-12 07:51:47 +0530

    SinglePlayerStartingPosition

---
+++ src/snackladder/SnackLadderSinglePlayer.java
@@ -0,0 +1,18 @@
+package snackladder;
+
+
+
+//Snake and Ladder
+//game played with
+//single player at start
+//position 0
+
+public class SnackLadderSinglePlayer {
+        public static void main(String [] args){
+            System.out.println("Welcome to snack ladder game");
+            int Player_position=0;
+            System.out.println("Playing single player");
+            System.out.println("current position is:" +Player_position);
+        }
+    }
+
